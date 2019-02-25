c99b6b12e657d2f3763ea86faf60edaaa9a7d477
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-02-24 21:38:04 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './components/Home';
+import AvengersList from './components/AvengersList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route exact path="/" component={Home} />  {/* the route is searched for by RegEx.  Router will therefore return whatever is the first match unless you use exact. */}
+        <Route exact path="/avengers" component={AvengersList} />
+
       </div>
     );
   }
---
+++ src/components/AvengersList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function AvengersList() {
+  return (
+    <div>
+      <h1>Avengers List</h1>
+      <ul>
+        <li>Thor</li>
+        <li>Hulk</li>
+        <li>Iron Man</li>
+        <li>Dr.Strange</li>
+        <li>Captain Merica</li>
+        <li>Black Panther</li>
+      </ul>
+    </div>
+  );
+}
+
+export default AvengersList;
